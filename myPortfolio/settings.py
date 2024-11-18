94e4f4a7eec9310e3bcd8a9cee850c6120ac4056
Muhammad Qasim <malikqasim20051@gmail.com>
2024-11-18 14:01:00 +0500

    commit

--- myPortfolio/settings.py
+++ myPortfolio/settings.py
@@ -137,7 +137,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'productionfiles'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static", 
+    BASE_DIR / "static",  
 ]
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # MEDIA_ROOT = BASE_DIR / 'media'
