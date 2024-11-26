2673ade0a76d438131279b306ad3081931f0eb10
Muhammad Qasim <malikqasim20051@gmail.com>
2024-11-26 15:17:22 +0500

    completed

--- Messages/models.py
+++ Messages/models.py
@@ -7,5 +7,6 @@ from django.db import models
 class Messages(models.Model):
     name = models.CharField(max_length=60)
     email = models.CharField(max_length=60)
+    # email = models.EmailField(max_length=60, blank=False, unique=True)  # Use EmailField for email
     messagesubject = models.CharField(max_length=60)
     message = models.TextField(max_length=500)
